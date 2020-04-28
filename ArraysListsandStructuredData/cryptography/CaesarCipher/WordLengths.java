8c6f3e1d0c08fb004c924a7cdb6f8c6b71bc91c1
auna <annah.ramones@gmail.com>
2020-04-27 20:10:51 -0700

    method countWordLengths fixed

--- ArraysListsandStructuredData/cryptography/CaesarCipher/WordLengths.java
+++ ArraysListsandStructuredData/cryptography/CaesarCipher/WordLengths.java
@@ -7,6 +7,50 @@ import edu.duke.*;
  */
 public class WordLengths {
     public void countWordLengths(FileResource resource, int[] counts){
+        //reads in words from the resource and count the number of words of each length
+        //for all the words in the resource, storing the counts in the array counts
+        //loop through the resource
+        for(String word: resource.words()){
+            //initialize string builder
+            StringBuilder modWord = new StringBuilder(word);
+            int lengthOfCurrWord = word.length();
+            //check if first and last character of word are letters            
+            boolean first = Character.isLetter(modWord.charAt(0));
+            boolean last = Character.isLetter(modWord.charAt(lengthOfCurrWord-1));
+            //if both the first and last characters are not letters,
+            if(!first && !last){
+                //the word at this index has a length of -2 of this index
+                counts[lengthOfCurrWord-2] +=1;
+                //System.out.println("The word " + word + " is " + (lengthOfCurrWord-2));
+                //System.out.println("\t First: " + first + "\tLast: " + last);
+            }
+            //else if, if both characters are letters
+            else if(first && last){
+                //the word at this index has a length of -1 of this index
+                counts[lengthOfCurrWord] += 1;
+                //System.out.println("The word " + word + " is " + (lengthOfCurrWord));
+                //System.out.println("\t First: " + first + "\tLast: " + last);
+            }
+            //else, catches the cases where only 1 is not a letter
+            else{
+                counts[lengthOfCurrWord-1] += 1;
+                //System.out.println("The word " + word + " is " + (lengthOfCurrWord-1));
+                //System.out.println("\t First: " + first + "\tLast: " + last);
+            }
+        }
         
+        
+        for(int k=0; k<counts.length; k++){
+            if(counts[k] != 0){
+                System.out.println(counts[k]+" words of length "+k);
+            }
+        }
+        
+    }
+    
+    public void testCountWordLengths(){
+        FileResource resource = new FileResource();
+        System.out.println();
+        countWordLengths(resource,new int[31]);
     }
 }
