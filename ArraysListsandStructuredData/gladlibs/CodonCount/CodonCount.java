b04bc84e4c4b23f4dbcb510aaa5c8854ecdc7917
auna <annah.ramones@gmail.com>
2020-04-29 15:56:31 -0700

    CodonCount class done

--- ArraysListsandStructuredData/gladlibs/CodonCount/CodonCount.java
+++ ArraysListsandStructuredData/gladlibs/CodonCount/CodonCount.java
@@ -13,10 +13,39 @@ public class CodonCount {
         map = new HashMap<String,Integer>();
     }
     
+    public void tester(){
+        FileResource fr = new FileResource();
+        String dnaStrand = fr.asString().toUpperCase().trim();
+        
+        System.out.println("------------");
+        //reading frame 0
+        
+        for(int rf=0;rf<3;rf++){
+            buildCodonMap(rf, dnaStrand);
+            String mostCommonCodon = getMostCommonCodon();
+            int start = 1; int end = 5;
+            System.out.println("Reading frame starting with " + rf + " results in " +
+                                map.size()+" unique codons");
+            //for (String s: map.keySet()){
+            //    System.out.println(s + "\t "+map.get(s));
+            //}
+            System.out.println("Most common codon is " + mostCommonCodon +
+                                " with count " + map.get(mostCommonCodon));
+            System.out.println("Counts of codons between " + start+" and "+ end +" inclusive are: ");
+            printCodonCounts(start,end);
+        }
+        System.out.println("------------");
+    }
+    
     public void printCodonCounts(int start, int end){
         //prints all codons in the hashmap whose counts are between start and end inclusive
         for (String s : map.keySet()){
-            
+            //get the count of the current codon
+            int currCount = map.get(s);
+            //if its between the start and end values, print it
+            if(currCount >= start && currCount <= end){
+                System.out.println(s +"\t"+currCount);
+            }
         }
     }
     
@@ -53,21 +82,25 @@ public class CodonCount {
         //will be building it multiple times, so need to clear map.
         map.clear();
         //loop through string, start at start index
-        for(int k=start; k<dna.length();k++){
+        for(int k=start; k<dna.length();k=k+3){
             //a codon has a length of 3, find end index and check if its within the length of the dna strand
-            int endIdx = start+2;
+            int endIdx = k+3;
+            //System.out.println("Start index: "+k+"\tEnd index: "+ endIdx);
             //if the end index is less than or equal to the length of the array, do things
             if (endIdx <= dna.length()){
                 //if it is, go ahead and find the codon and add it to the map
                 String codon = dna.substring(k,endIdx);
-                //check if its already in the map
-                if (!map.containsKey(codon)){
-                    //if its not there, put it in there
-                    map.put(codon,1);
-                }
-                //if its already there, increment the count
-                else{
-                    map.put(codon,map.get(codon)+1);
+                
+                if(codon.length() == 3){
+                    //check if its already in the map
+                    if (!map.containsKey(codon)){
+                        //if its not there, put it in there
+                        map.put(codon,1);
+                    }
+                    //if its already there, increment the count
+                    else{
+                        map.put(codon,map.get(codon)+1);
+                    }
                 }
             }
             //if not, do nothing
