699df9d1521f7d898527d7c1203747259b90eda6
auna <annah.ramones@gmail.com>
2020-04-29 13:05:09 -0700

    first pass at gladlibs, adding ability to keep a list of used words and not use them

---
+++ ArraysListsandStructuredData/gladlibs/GladLib/GladLib.java
@@ -0,0 +1,174 @@
+import edu.duke.*;
+import java.util.*;
+
+public class GladLib {
+    private ArrayList<String> adjectiveList;
+    private ArrayList<String> nounList;
+    private ArrayList<String> colorList;
+    private ArrayList<String> countryList;
+    private ArrayList<String> nameList;
+    private ArrayList<String> animalList;
+    private ArrayList<String> timeList;
+    private ArrayList<String> verbList;
+    private ArrayList<String> fruitList;
+    
+    private ArrayList<String> usedList;
+    
+    private Random myRandom;
+    
+    private static String dataSourceURL = "http://dukelearntoprogram.com/course3/data";
+    private static String dataSourceDirectory = "data";
+    
+    public GladLib(){
+        initializeFromSource(dataSourceDirectory);
+        myRandom = new Random();
+        usedList = new ArrayList<String>();
+        usedList.add("Ignore Initialization");
+    }
+    
+    
+    public GladLib(String source){
+        initializeFromSource(source);
+        myRandom = new Random();
+    }
+    
+    private void initializeFromSource(String source) {
+        adjectiveList= readIt(source+"/adjective.txt"); 
+        nounList = readIt(source+"/noun.txt");
+        colorList = readIt(source+"/color.txt");
+        countryList = readIt(source+"/country.txt");
+        nameList = readIt(source+"/name.txt");      
+        animalList = readIt(source+"/animal.txt");
+        timeList = readIt(source+"/timeframe.txt");
+        verbList = readIt(source+"/verb.txt");
+        fruitList = readIt(source+"/fruit.txt");
+    }
+    
+    private String randomFrom(ArrayList<String> source){
+        int index = myRandom.nextInt(source.size());
+        return source.get(index);
+    }
+    
+    private String getSubstitute(String label) {
+        if (label.equals("country")) {
+            return randomFrom(countryList);
+        }
+        if (label.equals("color")){
+            return randomFrom(colorList);
+        }
+        if (label.equals("noun")){
+            return randomFrom(nounList);
+        }
+        if (label.equals("name")){
+            return randomFrom(nameList);
+        }
+        if (label.equals("adjective")){
+            return randomFrom(adjectiveList);
+        }
+        if (label.equals("animal")){
+            return randomFrom(animalList);
+        }
+        if (label.equals("timeframe")){
+            return randomFrom(timeList);
+        }
+        if (label.equals("number")){
+            return ""+myRandom.nextInt(50)+5;
+        }
+        if (label.equals("verb")){
+            return randomFrom(verbList);
+        }
+        if (label.equals("fruit")){
+            return randomFrom(fruitList);
+        }
+        return "**UNKNOWN**";
+    }
+    
+    private String processWord(String w){
+        int first = w.indexOf("<");
+        int last = w.indexOf(">",first);
+        if (first == -1 || last == -1){
+            return w;
+        }
+        String prefix = w.substring(0,first);
+        String suffix = w.substring(last+1);
+        String sub = getSubstitute(w.substring(first+1,last));
+        //check if word is in the usedlist
+        boolean isItUsed = usedList.contains(sub);
+        //if not, add it to the list
+        if(!isItUsed){usedList.add(sub);}
+        //if it is, keep looking for another word
+        while(isItUsed){
+            //get a new sub word
+            String currSub = getSubstitute(w.substring(first+1,last));
+            //check if the new word is in the usedlist
+            boolean currUsed = usedList.contains(currSub);
+            //if its not, set that to sub variable, add it to the list and set isitused to false
+            if(!currUsed){ sub = currSub; usedList.add(currSub);isItUsed=false;}
+        }
+        
+        return prefix+sub+suffix;
+    }
+    
+    private void printOut(String s, int lineWidth){
+        int charsWritten = 0;
+        for(String w : s.split("\\s+")){
+            if (charsWritten + w.length() > lineWidth){
+                System.out.println();
+                charsWritten = 0;
+            }
+            System.out.print(w+" ");
+            charsWritten += w.length() + 1;
+        }
+    }
+    
+    private String fromTemplate(String source){
+        String story = "";
+        if (source.startsWith("http")) {
+            URLResource resource = new URLResource(source);
+            for(String word : resource.words()){
+                story = story + processWord(word) + " ";
+            }
+        }
+        else {
+            FileResource resource = new FileResource(source);
+            for(String word : resource.words()){
+                story = story + processWord(word) + " ";
+            }
+        }
+        return story;
+    }
+    
+    private ArrayList<String> readIt(String source){
+        ArrayList<String> list = new ArrayList<String>();
+        if (source.startsWith("http")) {
+            URLResource resource = new URLResource(source);
+            for(String line : resource.lines()){
+                list.add(line);
+            }
+        }
+        else {
+            FileResource resource = new FileResource(source);
+            for(String line : resource.lines()){
+                list.add(line);
+            }
+        }
+        return list;
+    }
+    
+    public void makeStory(){
+        System.out.println("\n");
+        System.out.println("---------");
+        String story = fromTemplate("datalong/madtemplate2.txt");
+        printOut(story, 60);
+        System.out.println();
+        System.out.println("The following is the list of used words");
+        for(int k=0; k<usedList.size();k++){
+            System.out.println(usedList.get(k)+"\t");
+        }
+        System.out.println("---------");
+        usedList.clear();
+    }
+    
+
+
+}
