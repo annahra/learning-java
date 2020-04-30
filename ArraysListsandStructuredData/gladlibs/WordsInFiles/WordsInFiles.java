0fb23881e8ae0fa73ad9a561cfcfc82283642ae0
auna <annah.ramones@gmail.com>
2020-04-29 18:12:18 -0700

    added ability to grab max number of times a word appears in a group of files

--- ArraysListsandStructuredData/gladlibs/WordsInFiles/WordsInFiles.java
+++ ArraysListsandStructuredData/gladlibs/WordsInFiles/WordsInFiles.java
@@ -15,8 +15,51 @@ public class WordsInFiles {
         map = new HashMap<String, ArrayList<String>>();
     }
     
-    private void buildWordFileMap(){
+    private ArrayList<String> wordsInNumFiles(int number){
+        //returns an arralylist of words that appear in exactly number of files
+        //initialize the array list
+        ArrayList<String> al = new ArrayList<String>();
+        //iterate through the map
         
+        
+        return null;
+    }
+    
+    private int maxNumber(){
+        //finds max number of time a word appears in a group of files
+        //initialize the max number
+        int max = 0;
+        //iterate through the map
+        for(String s : map.keySet()){
+            //for each word, grab the number of files that word shows up in
+            int currNum = map.get(s).size();
+            //if this is the first time the loop is running, initialize max to first value
+            if(max == 0){max = currNum;}
+            //for any other case
+            else{
+                //check if currNum is larger than max
+                if(currNum > max){
+                    //if it is, set that num as the max
+                    max = currNum;
+                }
+                //if it isnt dont do anyting
+            }
+        }
+        
+        
+        return max;
+    }
+    
+    private void buildWordFileMap(){
+        //clears map, then uses a directory resorce to get a group of files, then puts all the words
+        //in the file in the map by calling addWordsFromFile
+        map.clear();
+        DirectoryResource dr = new DirectoryResource();
+        //grab all the files from the directory resource
+        for(File f : dr.selectedFiles()){
+            //for each file, add the words in the word map
+            addWordsFromFile(f);
+        }
     
     }
     
